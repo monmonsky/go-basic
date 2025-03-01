99511b4b8b22004ada6642c97258ea9782f16802
monmonsky <hifajjar@gmail.com>
2025-03-01 11:18:45 +0700

    init

---
+++ basic/variable.go
@@ -0,0 +1,89 @@
+package main
+
+import (
+	"fmt"
+)
+
+// bisa di luar function
+// var name string = "monsky"
+// var age = 25
+
+// var (
+// 	name string = "monsky"
+// 	age         = 25
+// )
+
+// var name, age = "monsky", 25
+
+func main() {
+
+	// name, age := "nobeeid", 10
+
+	// // hanya boleh di dalam function
+	// user := "monsky"
+
+	// fmt.Println(name, age, user)
+
+	// // multiple variable
+	// text, twiceAge := get("main", age)
+	// fmt.Println("return from get", text, "age is ", twiceAge)
+
+	students := []string{"monsky", "nobeeid", "nobi"}
+
+	f := find(students)
+	isExist := f("monsky")
+	fmt.Println(isExist)
+}
+
+// return multiple value
+func get(text string, age int) (string, int) {
+	fmt.Println("ini function get", text, age*2)
+	post(10, 20, 30, 40, 50)
+
+	return text, age * 2
+}
+
+// variadic parameter
+func post(data ...int) {
+	fmt.Println("ini function post")
+
+	var total = 0
+	for i := 0; i < len(data); i++ {
+		total += data[i]
+	}
+
+	fmt.Println(total)
+
+	coba(10, timeTo20)
+	coba(20, func(i int) int {
+		return i * 3
+	})
+	coba(30, func(i int) int {
+		return i
+	})
+}
+
+// closure
+func timeTo20(i int) int {
+	return i * 20
+}
+
+// callback
+func coba(num int, cb func(int) int) {
+	total := cb(num)
+	fmt.Println("total callback", total)
+}
+
+// using callback
+func find(students []string) func(string) bool {
+	return func(s string) bool {
+		isExist := false
+		for i := 0; i < len(students); i++ {
+			if students[i] == s {
+				isExist = true
+				break
+			}
+		}
+		return isExist
+	}
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module nbfriends
+
+go 1.24.0
